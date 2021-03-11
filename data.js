158f018c99b96af4902a3487d362287223dfc3c6
outsidemom <60087235+outsidemom@users.noreply.github.com>
2021-03-11 08:12:20 -0600

    second 360

---
+++ data.js
@@ -0,0 +1,439 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-under-overhang",
+      "name": "under overhang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -1.4944776805391378,
+        "pitch": 0.10117028735067635,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2882002777711827,
+          "pitch": 0.08956464043520995,
+          "rotation": 0,
+          "target": "1-in-front-of-barn"
+        },
+        {
+          "yaw": -2.6622979410692427,
+          "pitch": 0.39049835147845613,
+          "rotation": 0,
+          "target": "7-man-door"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-in-front-of-barn",
+      "name": "In front of barn",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1733605915722336,
+          "pitch": 0.24046906248633348,
+          "rotation": 0,
+          "target": "7-man-door"
+        },
+        {
+          "yaw": -2.85475924958077,
+          "pitch": 0.09175002416207434,
+          "rotation": 0,
+          "target": "6-work-bench"
+        },
+        {
+          "yaw": -1.7960840915379155,
+          "pitch": 0.24783068536632413,
+          "rotation": 0,
+          "target": "0-under-overhang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.33214035110441387,
+          "pitch": 0.0841900461997458,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8032352839163126,
+          "pitch": -0.16086185126185448,
+          "title": "White truck",
+          "text": "The white truck way over there is my truck."
+        }
+      ]
+    },
+    {
+      "id": "2-inside-far-corner",
+      "name": "Inside far corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09368196126531458,
+          "pitch": 0.26158643065974907,
+          "rotation": 0,
+          "target": "3-deer-wall"
+        },
+        {
+          "yaw": 1.585914164833385,
+          "pitch": -0.07524958913389668,
+          "rotation": 0,
+          "target": "7-man-door"
+        },
+        {
+          "yaw": 0.8618555544723456,
+          "pitch": -0.06199510001526498,
+          "rotation": 0,
+          "target": "6-work-bench"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-deer-wall",
+      "name": "Deer wall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 1.7894183157602166,
+        "pitch": -0.5325008453849236,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.675866357369479,
+          "pitch": 0.17313451696201554,
+          "rotation": 0,
+          "target": "2-inside-far-corner"
+        },
+        {
+          "yaw": -1.9716982903522613,
+          "pitch": 0.3859398908600582,
+          "rotation": 0,
+          "target": "4-infront-of-car"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6917783769215777,
+          "pitch": -0.49311312774136695,
+          "title": "Deer Wall",
+          "text": "This is where we hang our deer that don't get mounted."
+        }
+      ]
+    },
+    {
+      "id": "4-infront-of-car",
+      "name": "Infront of car",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -3.024590181829561,
+        "pitch": 0.4179434436344689,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3207440208482346,
+          "pitch": 0.21292493060926887,
+          "rotation": 0,
+          "target": "3-deer-wall"
+        },
+        {
+          "yaw": 1.5586120776294763,
+          "pitch": 0.2481734853228943,
+          "rotation": 0,
+          "target": "5-behind-boat"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.092032562306315,
+          "pitch": 0.702048562278506,
+          "title": "Car",
+          "text": "This is our 2012 mustang. It is not stock. The motor is very upgraded and it has a procharger. It makes 800hp to the rear tire."
+        }
+      ]
+    },
+    {
+      "id": "5-behind-boat",
+      "name": "Behind boat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 1.0707774677685187,
+        "pitch": -0.16342693523757923,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.133827299314161,
+          "pitch": 0.2724940944059213,
+          "rotation": 0,
+          "target": "4-infront-of-car"
+        },
+        {
+          "yaw": 0.005157412460421895,
+          "pitch": 0.22247224534240928,
+          "rotation": 0,
+          "target": "6-work-bench"
+        },
+        {
+          "yaw": 0.8320906740718481,
+          "pitch": 0.005627728040185076,
+          "rotation": 0,
+          "target": "7-man-door"
+        },
+        {
+          "yaw": 2.5198810378670125,
+          "pitch": -0.10732999747488492,
+          "rotation": 0,
+          "target": "3-deer-wall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-work-bench",
+      "name": "work bench",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 2.2222402681766287,
+        "pitch": 0.02886787446898964,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.895580596299542,
+          "pitch": 0.22868346645435444,
+          "rotation": 0,
+          "target": "5-behind-boat"
+        },
+        {
+          "yaw": -0.7341036107169323,
+          "pitch": 0.2594226082362674,
+          "rotation": 0,
+          "target": "7-man-door"
+        },
+        {
+          "yaw": -0.3998108330317489,
+          "pitch": 0.2430700878153651,
+          "rotation": 0,
+          "target": "1-in-front-of-barn"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-man-door",
+      "name": "Man door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -2.482250802033837,
+        "pitch": -0.03722988997032317,
+        "fov": 1.3981561243886622
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.825224559953174,
+          "pitch": 0.027657277177315365,
+          "rotation": 0,
+          "target": "6-work-bench"
+        },
+        {
+          "yaw": -2.234902648797224,
+          "pitch": 0.11710264364119105,
+          "rotation": 0,
+          "target": "5-behind-boat"
+        },
+        {
+          "yaw": 3.007545540917773,
+          "pitch": 0.01786653728597365,
+          "rotation": 0,
+          "target": "2-inside-far-corner"
+        },
+        {
+          "yaw": 2.534297369246879,
+          "pitch": 0.1649122554074971,
+          "rotation": 0,
+          "target": "1-in-front-of-barn"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.736828437190196,
+          "pitch": 0.3411933281638788,
+          "title": "Junk shelf",
+          "text": "This is the shelf that you just throw extra junk on."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
